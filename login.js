f5323d35ebc57941b088fe5047cf2c9bd552cff1
Manishkumar2784 <manish.raj214@gmail.com>
2022-09-14 17:13:55 +0530

    all checked & verified

--- login.js
+++ login.js
@@ -1,11 +1,12 @@
 let num=13;
+let count=0;
 
  for (i=0;i<=num;i++){
    if (num%i==0){
       count++;
     }
 }
-   if (count==0){
+   if (count==2){
       console.log("Num is Prime")
   }else{
       console.log("Num is not prime)
