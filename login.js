85bbf410db842969b1d8b10a3070f8f67757620d
alokrai0607 <saelalok001@gmail.com>
2022-09-14 15:43:20 +0530

    Please Check and Verifiy

---
+++ login.js
@@ -0,0 +1,11 @@
+let num=13;
+
+ for (i=0;i<=num;i++){
+   if (num%i==0){
+      count++;
+    }
+}
+   if (count==0){
+      console.log("Num is Prime")
+  }else{
+      console.log("Num is not prime)
